f59c8d8c3fac21fc8960dfbe58621ef8e53c6a01
Kai <texasred2013@hotmail.com>
2016-04-12 12:29:41 -0500

    admin:add example code and dataset for the project

---
+++ examples/evaluation.py
@@ -0,0 +1,12 @@
+import math
+
+
+def computeNMSE(estBeta, origBeta):
+	estBetaSum = sum(estBeta)
+	origBetaSum = sum(origBeta)
+
+	nmse = 0
+	for i in xrange(len(estBeta)):
+		nmse += ((estBeta[i]/estBetaSum) - (origBeta[i]/origBetaSum)) ** 2
+
+	return nmse / len(estBeta)
