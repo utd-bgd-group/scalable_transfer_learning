bd1bcbd4d622dbc91db4d183ca64dadfdb31b0f2
Kai Huang <huangkai1988@gmail.com>
2016-04-17 18:59:52 -0500

    Update parallel.py

--- py_tutorial/parallel.py
+++ py_tutorial/parallel.py
@@ -1,11 +1,23 @@
+from __future__ import print_function
+from pyspark import SparkContext
 import numpy as np
 
-def f(iterator):
+sc = SparkContext(appName="PythonParallelProcessing")
+
+def get_sum(iterator):
     a = []
     for x in iterator:
         a.append(x)
-    print a
-    print 'sum= %i ' % np.sum(a)
-    yield 10
+    # print (a)
+    return np.sum(a)
 
-sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3).foreachPartition(f)
+def main():
+    rdd = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)
+    # rdd.foreachPartition(do something)
+    res = rdd.mapPartitions(get_sum, True).collect()
+  
+    resRdd = sc.parallelize(res)
+    resRdd.saveAsTextFile('hdfs://cshadoop1/kxh132430/py_test/out_1')
+
+if __name__ == '__main__':
+	main()
