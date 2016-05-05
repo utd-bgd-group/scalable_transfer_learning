6c57984c65bf22f7af99dab592b76caa2fe8eab6
Kai Huang <texasred2013@hotmail.com>
2016-05-04 19:19:18 -0500

    kaihuang:I set the bag size to be configurable

--- run.py
+++ run.py
@@ -10,12 +10,16 @@ from lib.kmm import computeBeta
 from lib.evaluation import computeNMSE
 
 
+def get_bag_size(bag_size, data, sample_no):
+    return bag_size if bag_size else len(data) / sample_no
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', "--bagging", type=int, choices=[1,2,3,4], default=1, help="bagging strategy")
     parser.add_argument("-t", "--training", type=int, default=12000, help="size of trainning data")
     parser.add_argument("-r", "--reverse", action="store_true", help="set -t as the size of test data")
-    parser.add_argument("-s", "--bag_size", type=int, default=600, help="the sample size")
+    parser.add_argument("-s", "--bag_size", type=int, help="the sample size")
     parser.add_argument("-m", "--train_samples", type=int, default=1, help="number of samples from training")
     parser.add_argument("-n", "--test_samples", type=int, default=1, help="number of samples from test")
     parser.add_argument("-i", "--input", type=str, default='./dataset/powersupply.arff', help="default input file")
@@ -26,7 +30,7 @@ def main():
     mode = args.bagging # bagging strategy
     training_size = args.training # training set size (small training set)
     reverse = args.reverse # flip training to test (small test set)
-    bag_size = args.bag_size # the bag_size
+    bag_size = args.bag_size # By default, the bag size is dynamic, if specified, the bag size will fix
     m = args.train_samples # take m samples from training
     n = args.test_samples # take n samples from test
     input_file = args.input # input file path
@@ -38,23 +42,28 @@ def main():
     test_data = np.array(test)
     orig_beta_data = np.array(train_beta)
 
-    # statistics
-    statistics = "mode=%s, train_size=%i, test_size=%i, bag_size=%i, m=%i, n=%i\n" % (mode, len(train_data), len(test_data), bag_size, m, n)
-
+    tr_bag_size = len(train_data)
+    te_bag_size = len(test_data)
     # Generate the bagging index using different bagging strategies
     cartesian_sampling = True
     if mode == 1: # take m samples from train set and entire test set
-        tr_n = bag(train_data, size=bag_size, sample_no=m)
+        tr_bag_size = get_bag_size(bag_size, train_data, m)
+        tr_n = bag(train_data, size=tr_bag_size, sample_no=m)
         te_n = [range(len(test_data))]
     elif mode == 2: # take entire train set and n samples from test set
         tr_n = [range(len(train_data))]
-        te_n = bag(test_data, size=bag_size, sample_no=n)
+        te_bag_size = get_bag_size(bag_size, test_data, n)
+        te_n = bag(test_data, size=te_bag_size, sample_no=n)
     elif mode == 3: # take m from train set and n samples from test set, do cartesian product pairing
-        tr_n = bag(train_data, size=bag_size, sample_no=m)
-        te_n = bag(test_data, size=bag_size, sample_no=n)
+        tr_bag_size = get_bag_size(bag_size, train_data, m)
+        te_bag_size = get_bag_size(bag_size, test_data, n)
+        tr_n = bag(train_data, size=tr_bag_size, sample_no=m)
+        te_n = bag(test_data, size=te_bag_size, sample_no=n)
     elif mode == 4: # take min(m,n) samples each from train set and test set, do one-to-one pairing
-        tr_n = bag(train_data, size=bag_size, sample_no=m)
-        te_n = bag(test_data, size=bag_size, sample_no=n)
+        tr_bag_size = get_bag_size(bag_size, train_data, m)
+        te_bag_size = get_bag_size(bag_size, test_data, n)
+        tr_n = bag(train_data, size=tr_bag_size, sample_no=m)
+        te_n = bag(test_data, size=te_bag_size, sample_no=n)
         cartesian_sampling = False
     else:
         print 'Invalid mode!'
@@ -82,6 +91,10 @@ def main():
     orig_beta = orig_beta_data[est_beta_idx]
     final_result = computeNMSE(est_beta, orig_beta)
 
+    # statistics
+    statistics = "mode=%s, train_size=%i, test_size=%i, bag_size=%s, m=%i, n=%i\n" % \
+                 (mode, len(train_data), len(test_data), bag_size if bag_size else [tr_bag_size, te_bag_size], m, n)
+
     # Save the result into a text file
     with open(output_file, 'a') as output_file:
         message = "The final NMSE is : %s \n" % final_result
@@ -91,4 +104,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
