f16ca7c6ff014144ff7fa8b26a036ae8cc7ee20e
Kai Huang <texasred2013@hotmail.com>
2016-05-05 00:10:12 -0500

    kaihuang: support fix sample size and sample no, add timing feature

--- run.py
+++ run.py
@@ -2,16 +2,25 @@
 from pyspark import SparkContext
 sc = SparkContext(appName="PythonProject", pyFiles=['lib.zip'])
 import numpy as np
-import sys
 import argparse
+import time
 from lib.splitter import split
 from lib.bagger import bag, pair, cartesian
 from lib.kmm import computeBeta
 from lib.evaluation import computeNMSE
 
 
-def get_bag_size(bag_size, data, sample_no):
-    return bag_size if bag_size else len(data) / sample_no
+def get_size_no(data, bag_size, sample_no):
+    if bag_size:
+        if sample_no:
+            return bag_size, sample_no
+        else:
+            return bag_size, len(data) / bag_size
+    else:
+        if sample_no:
+            return len(data) / sample_no, sample_no
+        else:
+            return len(data), 1
 
 
 def main():
@@ -36,48 +45,38 @@ def main():
     input_file = args.input # input file path
     output_file = args.output # output file path
 
-    # Generate biased train and test set, as well as the orginal beta for train set
+    # Step 1: Generate biased train and test set, as well as the orginal beta for train set
+    start = time.time()
+
     train, train_beta, test = split(input_file, training_size, reverse)
     train_data = np.array(train)
     test_data = np.array(test)
     orig_beta_data = np.array(train_beta)
 
-    tr_bag_size = len(train_data)
-    te_bag_size = len(test_data)
-    # Generate the bagging index using different bagging strategies
-    cartesian_sampling = True
-    if mode == 1: # take m samples from train set and entire test set
-        tr_bag_size = get_bag_size(bag_size, train_data, m)
-        tr_n = bag(train_data, size=tr_bag_size, sample_no=m)
-        te_n = [range(len(test_data))]
-    elif mode == 2: # take entire train set and n samples from test set
-        tr_n = [range(len(train_data))]
-        te_bag_size = get_bag_size(bag_size, test_data, n)
-        te_n = bag(test_data, size=te_bag_size, sample_no=n)
-    elif mode == 3: # take m from train set and n samples from test set, do cartesian product pairing
-        tr_bag_size = get_bag_size(bag_size, train_data, m)
-        te_bag_size = get_bag_size(bag_size, test_data, n)
-        tr_n = bag(train_data, size=tr_bag_size, sample_no=m)
-        te_n = bag(test_data, size=te_bag_size, sample_no=n)
-    elif mode == 4: # take min(m,n) samples each from train set and test set, do one-to-one pairing
-        tr_bag_size = get_bag_size(bag_size, train_data, m)
-        te_bag_size = get_bag_size(bag_size, test_data, n)
-        tr_n = bag(train_data, size=tr_bag_size, sample_no=m)
-        te_n = bag(test_data, size=te_bag_size, sample_no=n)
-        cartesian_sampling = False
-    else:
-        print 'Invalid mode!'
-        sys.exit(-1)
+    end = time.time()
+    split_time = end - start
+
+    # Step 2: Generate the bagging index using different bagging strategies
+    start = time.time()
 
     # Bagging the train and test data from the sampled index
-    if cartesian_sampling:
+    tr_bag_size, tr_bag_no = get_size_no(train_data, bag_size, m)
+    te_bag_size, te_bag_no = get_size_no(test_data, bag_size, n)
+    tr_n = bag(train_data, size=tr_bag_size, sample_no=tr_bag_no)
+    te_n = bag(test_data, size=te_bag_size, sample_no=te_bag_no)
+
+    if mode < 4:
         bags = cartesian(train_data, test_data, tr_n, te_n)
     else:
-        bags = pair(train_data, test_data, tr_n, te_n, sample_no=min(m, n))
-
+        bags = pair(train_data, test_data, tr_n, te_n, sample_no=min(tr_bag_no, te_bag_no))
     rdd = sc.parallelize(bags)
 
-    # Compute the estimated beta
+    end = time.time()
+    bagging_time = end - start
+
+    # Step 3: Compute the estimated beta
+    start = time.time()
+
     res = rdd.map(lambda (idx, tr, te): computeBeta(idx, tr, te)).flatMap(lambda x: x)
 
     rdd1 = res.aggregateByKey((0,0), lambda a,b: (a[0] + b, a[1] + 1),
@@ -86,20 +85,34 @@ def main():
     est_beta_map = rdd1.mapValues(lambda v: v[0]/v[1]).collectAsMap()
     est_beta_idx = est_beta_map.keys()
 
-    # Compute the NMSE between the est_beta and orig_beta
+    end = time.time()
+    compute_time = end - start
+
+    # Step 4: Compute the NMSE between the est_beta and orig_beta
+    start = time.time()
+
     est_beta = [est_beta_map[x] for x in est_beta_idx]
     orig_beta = orig_beta_data[est_beta_idx]
     final_result = computeNMSE(est_beta, orig_beta)
 
+    end = time.time()
+    evaluate_time = end - start
+
     # statistics
-    statistics = "mode=%s, train_size=%i, test_size=%i, bag_size=%s, m=%i, n=%i\n" % \
-                 (mode, len(train_data), len(test_data), bag_size if bag_size else [tr_bag_size, te_bag_size], m, n)
+    statistics = "mode=%s, train_size=%i, test_size=%i, tr_bag_size=%i, m=%i, te_bag_size=%i, n=%i\n" % \
+                 (mode, len(train_data), len(test_data), tr_bag_size, tr_bag_no, te_bag_size, te_bag_no)
+    total_time = split_time + bagging_time + compute_time + evaluate_time
+    time_info = "split_time=%s, bagging_time=%s, compute_time=%s, evaluate_time=%s, total_time=%s\n" % \
+                (split_time, bagging_time, compute_time, evaluate_time, total_time)
+    print statistics
+    print time_info
 
     # Save the result into a text file
     with open(output_file, 'a') as output_file:
         message = "The final NMSE is : %s \n" % final_result
         print message
         output_file.write(statistics)
+        output_file.write(time_info)
         output_file.write(message)
 
 if __name__ == '__main__':
